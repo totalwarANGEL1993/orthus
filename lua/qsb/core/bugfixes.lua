334465da144318da6cb825fa2eb39bd618cc6783
Martin Heller <n3crosh8d3@gmail.com>
2020-12-08 20:54:39 +0100

    implements find view fix

--- lua/qsb/core/bugfixes.lua
+++ lua/qsb/core/bugfixes.lua
@@ -8,8 +8,9 @@
 -- This module fixes some minor bugs in the game.
 --
 -- <h4>Crush Building</h4>
--- Removes the confirmation menu from destroy building button. The building is
--- deselected and then immedaitly destroyed.
+-- When a building is crushed it is deselected before the command is executed.
+-- This way it is (for humans) impossilbe to reselect the building in time to
+-- use the crush building exploit.
 --
 -- <h4>Formations</h4>
 -- Formations no longer additionally need GT_Tactics to be allowed to be shown.
@@ -35,10 +36,11 @@
 --
 
 Bugfixes = {
-    UseCrushBuilding = true,
-    UseFormation = true,
+    UseCrushBuildingFix = true,
+    UseFormationFix = true,
     UseBlessLimit = false,
     UseWeatherChangeLimit = false,
+    UseFindViewFix = true,
 
     ScriptEvents = {},
     WeatherChangeLimit = {
@@ -53,6 +55,24 @@ Bugfixes = {
         BlessSettlers4 = {};
         BlessSettlers5 = {};
     },
+
+    Maps = {
+        EntityCategoryToFindView = {
+            ["Sword"]        = "FindSwordmen",
+            ["Spear"]        = "FindSpearmen",
+            ["Bow"]          = "FindBowmen",
+            ["CavalryLight"] = "FindLightCavalry",
+            ["CavalryHeavy"] = "FindHeavyCavalry",
+            ["Rifle"]        = "FindRiflemen",
+        },
+        BlessCategoryToButton = {
+            [BlessCategories.Construction] = "BlessSettlers1",
+            [BlessCategories.Research]     = "BlessSettlers2",
+            [BlessCategories.Weapons]      = "BlessSettlers3",
+            [BlessCategories.Financial]    = "BlessSettlers4",
+            [BlessCategories.Canonisation] = "BlessSettlers5",
+        }
+    },
 };
 
 -- -------------------------------------------------------------------------- --
@@ -63,7 +83,7 @@ Bugfixes = {
 -- @within Methods
 --
 function ActivateCrushBuildingBugfix(_Flag)
-    Bugfixes.UseCrushBuilding = _Flag == true;
+    Bugfixes.UseCrushBuildingFix = _Flag == true;
 end
 
 ---
@@ -72,7 +92,7 @@ end
 -- @within Methods
 --
 function ActivateFormationBugfix(_Flag)
-    Bugfixes.UseFormation = _Flag == true;
+    Bugfixes.UseFormationFix = _Flag == true;
 end
 
 ---
@@ -91,6 +111,15 @@ end
 --
 function ActivateWeatherChangeLimitBugfix(_Flag)
     Bugfixes.UseWeatherChangeLimit = _Flag == true;
+end
+
+---
+-- Activates or deactivates the find view fix.
+-- @param[type=boolean] _Flag Active
+-- @within Methods
+--
+function ActivateFindViewBugfix(_Flag)
+    Bugfixes.UseFindViewFix = _Flag == true;
 end
 
 ---
@@ -122,12 +151,17 @@ end
 --
 function Bugfixes:Install()
     Tools.GiveResources = Tools.GiveResouces;
+    Mission_OnSaveGameLoaded_Orig_QSB_Bugfixes = Mission_OnSaveGameLoaded;
+    Mission_OnSaveGameLoaded = function()
+        Mission_OnSaveGameLoaded_Orig_QSB_Bugfixes();
+        Bugfixes:OverrideGuiSellBuilding();
+    end
     
+    self:OverrideGuiSellBuilding();
     self:CreateScriptEvents();
     self:OverrideGUIActions();
     self:OverrideGUITooltip();
     self:OverrideGUIUpdate();
-    self:OverrideGameCallback();
 end
 
 function Bugfixes:CreateScriptEvents()
@@ -147,46 +181,22 @@ function Bugfixes:PostWeatherChanged(_Time)
     MPSync:SnchronizedCall(self.ScriptEvents.PostWeatherChanged, _Time);
 end
 
-function Bugfixes:OverrideGUIActions()
-    -- Crush building fix
-    GUIAction_ToDestroyBuildingWindow_Orig_QSB_Bugfix = GUIAction_ToDestroyBuildingWindow;
-    GUIAction_ToDestroyBuildingWindow = function()
-        if Bugfixes.UseCrushBuilding then
-            local BuildingID = GUI.GetSelectedEntity();
-            if IsExisting(BuildingID) then
-                GUI.DeselectEntity(BuildingID);
-                GUI.SellBuilding(BuildingID);
-            end
-            return;
+function Bugfixes:OverrideGuiSellBuilding()
+    Bugfixes.GUI_SellBuilding = nil;
+    Bugfixes.GUI_SellBuilding = GUI.SellBuilding;
+    GUI.SellBuilding = function(_BuildingID)
+        if Bugfixes.UseCrushBuildingFix then
+            GUI.DeselectEntity(_BuildingID);
         end
-        GUIAction_ToDestroyBuildingWindow_Orig_QSB_Bugfix();
+        Bugfixes.GUI_SellBuilding(_BuildingID);
     end
+end
 
+function Bugfixes:OverrideGUIActions()
     -- Bless Settlers delay
     GUIAction_BlessSettlers = function(_BlessCategory)
         local PlayerID = GUI.GetPlayerID();
         local Time = math.floor(Logic.GetTime());
-        local Bless = "BlessSettlers1";
-        if _BlessCategory == BlessCategories.Research then
-            Bless = "BlessSettlers2";
-        end
-        if _BlessCategory == BlessCategories.Weapons then
-            Bless = "BlessSettlers3";
-        end
-        if _BlessCategory == BlessCategories.Financial then
-            Bless = "BlessSettlers4";
-        end
-        if _BlessCategory == BlessCategories.Canonisation then
-            Bless = "BlessSettlers5";
-        end
-
-        if Bugfixes.UseBlessLimit then
-            if Bugfixes:GetBlessDelayForPlayer(PlayerID, Bless) > 0 then
-                GUI.AddNote(XGUIEng.GetStringTableText("InGameMessages/GUI_NotEnoughFaith"));
-                Sound.PlayFeedbackSound(Sounds.VoicesMentor_INFO_MonksNeedMoreTime_rnd_01, 0);
-                return;
-            end
-        end
         if InterfaceTool_IsBuildingDoingSomething(GUI.GetSelectedEntity()) == true then		
             return;
         end
@@ -197,20 +207,16 @@ function Bugfixes:OverrideGUIActions()
             Sound.PlayFeedbackSound(Sounds.VoicesMentor_INFO_MonksNeedMoreTime_rnd_01, 0);
             return;
         end
-        Bugfixes:PostPlayerBlessed(PlayerID, Time, Bless);
+        Bugfixes:PostPlayerBlessed(
+            PlayerID,
+            Time,
+            Bugfixes.Maps.BlessCategoryToButton[_BlessCategory]
+        );
         GUI.BlessByBlessCategory(_BlessCategory);
     end
 
     -- Change weather delay
     GUIAction_ChangeWeather = function(_Weathertype)
-        if Bugfixes.UseBlessLimit then
-            local Data = Bugfixes.WeatherChangeLimit;
-            local Time = Data.Last + Data.Limit - math.floor(Logic.GetTime());
-            if Data.Last > 0 and Time > 0 then
-                GUI.AddNote(XGUIEng.GetStringTableText("InGameMessages/GUI_WeathermashineNotReady"));
-                return;
-            end
-        end
         if Logic.IsWeatherChangeActive() == true then
             GUI.AddNote(XGUIEng.GetStringTableText("InGameMessages/Note_WeatherIsCurrentlyChanging"))		
             return;
@@ -241,6 +247,7 @@ function Bugfixes:OverrideGUITooltip()
         if Bugfixes.UseBlessLimit then
             local Time = Bugfixes:GetBlessDelayForPlayer(PlayerID, Button);
             if Time > 0 then
+                XGUIEng.SetText(gvGUI_WidgetID.TooltipBottomText, XGUIEng.GetStringTableText(_Normal));
                 XGUIEng.SetText(gvGUI_WidgetID.TooltipBottomCosts, QuestSystem:ReplacePlaceholders("{red}" ..Time));
             end
         end
@@ -252,11 +259,11 @@ function Bugfixes:OverrideGUITooltip()
         GUITooltip_ResearchTechnologies_Orig_QSB_Bugfix(_Technology, _Text, _Key);
         local PlayerID = GUI.GetPlayerID();
         local WidgetID = XGUIEng.GetCurrentWidgetID();
-
         if Bugfixes.UseWeatherChangeLimit then
             local Data = Bugfixes.WeatherChangeLimit;
             local Time = Data.Last + Data.Limit - math.floor(Logic.GetTime());
             if Data.Last > 0 and Time > 0 then
+                XGUIEng.SetText(gvGUI_WidgetID.TooltipBottomText, XGUIEng.GetStringTableText(_Text));
                 XGUIEng.SetText(gvGUI_WidgetID.TooltipBottomCosts, QuestSystem:ReplacePlaceholders("{red}" ..Time));
             end
         end
@@ -264,55 +271,99 @@ function Bugfixes:OverrideGUITooltip()
 end
 
 function Bugfixes:OverrideGUIUpdate()
-    GUIUpdate_BuildingButtons_Orig_QSB_Bugfix = GUIUpdate_BuildingButtons;
-    GUIUpdate_BuildingButtons = function(_Button, _Technology)
-        GUIUpdate_BuildingButtons_Orig_QSB_Bugfix(_Button, _Technology);
+    GUIUpdate_FindView_Orig_QSB_Bugfix = GUIUpdate_FindView
+    GUIUpdate_FindView = function()
         local PlayerID = GUI.GetPlayerID();
-        local WidgetID = XGUIEng.GetCurrentWidgetID();
+        local EntityID = GUI.GetSelectedEntity();
 
-        -- Formation fix
-        if Bugfixes.UseFormation then
-            if string.find(_Button, "Formation0") then
-                XGUIEng.ShowWidget(_Button, 1);
-                if Logic.IsTechnologyResearched(PlayerID, Technologies.GT_StandingArmy) == 1 then
-                    XGUIEng.DisableButton(_Button, 0);
-                else
-                    XGUIEng.DisableButton(_Button, 1);
+        -- Find view fix
+        Bugfixes:UpdateFindViewButtons();
+        -- Find View
+        Bugfixes:UpdateFormationButtons();
+        -- Bless limit
+        Bugfixes:UpdateBlessSettlerButtons();
+        -- Weather change limit
+        Bugfixes:UpdateChangeWeatherButtons();
+    end
+end
+
+function Bugfixes:UpdateChangeWeatherButtons()
+    if Bugfixes.UseWeatherChangeLimit then
+        local Data = Bugfixes.WeatherChangeLimit;
+        local Time = Data.Last + Data.Limit - math.floor(Logic.GetTime());
+        GUIUpdate_ChangeWeatherButtons("WeatherTower_MakeSummer", Technologies.T_MakeSummer, 1);
+        GUIUpdate_ChangeWeatherButtons("WeatherTower_MakeRain", Technologies.T_MakeRain, 1);
+        GUIUpdate_ChangeWeatherButtons("WeatherTower_MakeSnow", Technologies.T_MakeSnow, 1);
+        if Data.Last > 0 and Time > 0 then
+            XGUIEng.DisableButton("WeatherTower_MakeSummer", 1);
+            XGUIEng.DisableButton("WeatherTower_MakeRain", 1);
+            XGUIEng.DisableButton("WeatherTower_MakeSnow", 1);
+        end
+    end
+end
+
+function Bugfixes:UpdateFindViewButtons()
+    local PlayerID = GUI.GetPlayerID();
+    local EntityID = GUI.GetSelectedEntity();
+    if Bugfixes.UseFindViewFix then
+        local ScoutAmount = 0;
+        if Entities.PU_Scout then
+            ScoutAmount = Logic.GetNumberOfEntitiesOfTypeOfPlayer(PlayerID, Entities.PU_Scout);
+        end
+        local ThiefAmount = 0;
+        if Entities.PU_Thief then
+            ThiefAmount = Logic.GetNumberOfEntitiesOfTypeOfPlayer(PlayerID, Entities.PU_Thief);
+        end
+        local AllCannons  = QSBTools.GetAllCannons(PlayerID);
+        local AllLeader   = QSBTools.GetAllLeader(PlayerID);
+
+        local ExistingMap = {}
+        for i= 1, table.getn(AllLeader), 1 do
+            for k, v in pairs(Bugfixes.Maps.EntityCategoryToFindView) do
+                if Logic.IsEntityInCategory(AllLeader[i], EntityCategories[k]) == 1 then
+                    ExistingMap[k] = (ExistingMap[k] or 0) +1;
+                end
+            end
+        end
+        for k, v in pairs(Bugfixes.Maps.EntityCategoryToFindView) do
+            XGUIEng.ShowWidget(
+                Bugfixes.Maps.EntityCategoryToFindView[k],
+                ((ExistingMap[k] or 0) > 0 and 1) or 0
+            );
+        end
+        XGUIEng.ShowWidget("FindCannon", (table.getn(AllCannons) > 0 and 1) or 0);
+        XGUIEng.ShowWidget("FindScout", (ScoutAmount > 0 and 1) or 0);
+        XGUIEng.ShowWidget("FindThief", (ThiefAmount > 0 and 1) or 0);
+    end
+end
+
+function Bugfixes:UpdateBlessSettlerButtons()
+    local PlayerID = GUI.GetPlayerID();
+    local EntityID = GUI.GetSelectedEntity();
+    if Bugfixes.UseBlessLimit then
+        if EntityID then
+            local EntityTypeName = Logic.GetEntityTypeName(Logic.GetEntityType(EntityID));
+            if string.find(EntityTypeName, "PB_Monastery") then
+                GUIUpdate_BuildingButtons("BlessSettlers1", Technologies.T_BlessSettlers1);
+                GUIUpdate_BuildingButtons("BlessSettlers2", Technologies.T_BlessSettlers2);
+                GUIUpdate_GlobalTechnologiesButtons("BlessSettlers3", Technologies.T_BlessSettlers3,Entities.PB_Monastery2);
+                GUIUpdate_GlobalTechnologiesButtons("BlessSettlers4", Technologies.T_BlessSettlers4,Entities.PB_Monastery2);
+                GUIUpdate_GlobalTechnologiesButtons("BlessSettlers5", Technologies.T_BlessSettlers5,Entities.PB_Monastery3);
+                for i= 1, 5, 1 do
+                    if Bugfixes:GetBlessDelayForPlayer(PlayerID, "BlessSettlers" ..i) > 0 then
+                        XGUIEng.DisableButton("BlessSettlers" ..i, 1);
+                    end
                 end
             end
         end
     end
 end
 
-function Bugfixes:OverrideGameCallback()
-    GameCallback_GUI_SelectionChanged_Orig_QSB_Bugfix = GameCallback_GUI_SelectionChanged;
-    GameCallback_GUI_SelectionChanged = function()
-        GameCallback_GUI_SelectionChanged_Orig_QSB_Bugfix();
-        Bugfixes:UpdateFormation();
-    end
-
-    GameCallback_OnBuildingConstructionComplete_Orig_QSB_Bugfix = GameCallback_OnBuildingConstructionComplete;
-    GameCallback_OnBuildingConstructionComplete = function(_BuildingID, _PlayerID)
-        GameCallback_GUI_SelectionChanged_Orig_QSB_Bugfix(_BuildingID, _PlayerID);
-        Bugfixes:UpdateFormation();
-    end
-
-    GameCallback_OnBuildingUpgradeComplete_Orig_QSB_Bugfix = GameCallback_OnBuildingUpgradeComplete;
-    GameCallback_OnBuildingUpgradeComplete = function(_BuildingIDOld, _BuildingIDNew)
-        GameCallback_OnBuildingUpgradeComplete_Orig_QSB_Bugfix(_BuildingIDOld, _BuildingIDNew);
-        Bugfixes:UpdateFormation();
-    end
-
-    GameCallback_OnTechnologyResearched_Orig_QSB_Bugfix = GameCallback_OnBuildingConstructionComplete;
-    GameCallback_OnTechnologyResearched = function(_PlayerID, _TechnologyType)
-        GameCallback_OnTechnologyResearched_Orig_QSB_Bugfix(_PlayerID, _TechnologyType);
-        Bugfixes:UpdateFormation();
-    end
-end
-
-function Bugfixes:UpdateFormation()
+function Bugfixes:UpdateFormationButtons()
+    local PlayerID = GUI.GetPlayerID();
     local EntityID = GUI.GetSelectedEntity();
-    if Bugfixes.UseFormation then
+    if Bugfixes.UseFormationFix then
+        -- Vilibility
         if IsExisting(EntityID) and Logic.IsLeader(EntityID) == 1
         and Logic.LeaderGetMaxNumberOfSoldiers(EntityID) > 0 then
             local TypeID = Logic.GetEntityType(EntityID);
@@ -326,6 +377,15 @@ function Bugfixes:UpdateFormation()
                 for i= 1, 4, 1 do
                     XGUIEng.ShowWidget("Formation0" ..i, 1);
                 end
+            end
+        end
+        -- Availability
+        for i= 1, 4, 1 do
+            XGUIEng.ShowWidget("Formation0" ..i, 1);
+            if Logic.IsTechnologyResearched(PlayerID, Technologies.GT_StandingArmy) == 1 then
+                XGUIEng.DisableButton("Formation0" ..i, 0);
+            else
+                XGUIEng.DisableButton("Formation0" ..i, 1);
             end
         end
     end
