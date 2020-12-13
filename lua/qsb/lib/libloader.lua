33ef727a0862d9d076d73b2258dcdf896d5624e0
Martin Heller <n3crosh8d3@gmail.com>
2020-12-13 20:26:08 +0100

    adds metatable fix

--- lua/qsb/lib/libloader.lua
+++ lua/qsb/lib/libloader.lua
@@ -4,6 +4,23 @@
 -- #    Author:   totalwarANGEL                                             # --
 -- ########################################################################## --
 
+---
+-- Loads libraries used by the QSB if they are not already present.
+--
+-- Libraries loaded (in this order):
+-- <ul>
+-- <li>OOP - Creates classes based on deep table copies.</li>
+-- <li>QSBTools - Several helper functions.</li>
+-- <li>LuaBit - Lua 5.0 implementation of lua bit.</li>
+-- <li>MD5 - Library to create simple MD5 hashes.</li>
+-- <li>Metatable (S5 Community) - Makes metatables savable.</li>
+-- <li>S5Hook (S5 Community) - Allows basic memory manipulation (not compatible
+-- to History Edition).</li>
+-- </ul>
+--
+-- @set sort=true
+--
+
 -- Load oop lib
 if inherit == nil then
     Script.Load(gvBasePath.. "core/oop.lua");
@@ -19,6 +36,10 @@ end
 -- Load MD5 hash generator lib
 if md5 == nil then
     Script.Load(gvBasePath.. "lib/md5.lua");
+end
+-- Load Metatable fix
+if metatable == nil then
+    Script.Load(gvBasePath.. "lib/metatable.lua");
 end
 -- Load S5Hook
 if S5HookData == nil then
